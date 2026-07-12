c82f7449e4a299839ee2c254dd4c74806bedd6b3
Otgonzul Zorigt <otgonzul.udachi2020@gmail.com>
2026-07-12 12:03:46 +0900

    Delete src directory
    Update

--- src/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-from .split import scaffold_split, scaffold_train_val_test_split
-from .smiles import (
-    filter_valid, encode_smiles, SMILESDataset, make_gru_loaders
-)
-from .graph import (
-    smiles_to_graph, build_graph_list,
-    CrossAttnDataset, crossattn_collate
-)
--- src/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .gru import GRUEncoder, GRUModel
-from .gin import GINEncoder, GINModel
-from .molbcat import MolBCAT
